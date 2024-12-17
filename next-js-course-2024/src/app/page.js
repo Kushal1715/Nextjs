5e1256e5cdd0349adf5d52bd85a672b6ceaaade4
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-17 20:59:18 +0545

    navigate using next/link

--- next-js-course-2024/src/app/page.js
+++ next-js-course-2024/src/app/page.js
@@ -1,8 +1,11 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="justify-items-center min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
       <h1>welcome to next js course</h1>
+      <Link href='/account'><button>navigate to account papge</button></Link>
+      <Link href='/products'><button>navigate to products papge</button></Link>
     </div>
   );
 }
