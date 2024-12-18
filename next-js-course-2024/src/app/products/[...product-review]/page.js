58547572520301279cb750e7b8d8ea66f8dc839e
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-18 21:13:35 +0545

    params component in catch all routes inside server component

--- next-js-course-2024/src/app/products/[...product-review]/page.js
+++ next-js-course-2024/src/app/products/[...product-review]/page.js
@@ -1,3 +1,5 @@
-export default function ProductReview() {
+export default async function ProductReview(props) {
+  const param = await props.params;
+  console.log(param)
   return <h1>this is product review page and catch all routes</h1>
 }
