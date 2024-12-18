e6f436481e063ac202afc7a29d841c77ae31fc3e
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-18 19:40:57 +0545

    redirect in server components

--- next-js-course-2024/src/app/account/page.js
+++ next-js-course-2024/src/app/account/page.js
@@ -1,6 +1,9 @@
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 const page = () => {
+  const userInfo = null;
+  if (userInfo === null) redirect('/profile')
   return (
     <div>Account</div>
   )
---
+++ next-js-course-2024/src/app/profile/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>this is profile papge</div>
+  )
+}
+
+export default page
