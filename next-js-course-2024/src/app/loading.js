b72c7f15a27bae882de4c7d2d2c66ce29685e82a
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-19 19:13:56 +0545

    loading ui

---
+++ next-js-course-2024/src/app/loading.js
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <h1 className="text-center text-5xl">Loading.. please wait</h1>
+}
--- next-js-course-2024/src/app/products/page.js
+++ next-js-course-2024/src/app/products/page.js
@@ -3,7 +3,7 @@ import React from "react";
 const Products = async (props) => {
   const searchParams = await props.searchParams;
   console.log(searchParams);
-  return <div>Products</div>;
+  return <div className="text-center text-5xl">Products</div>;
 };
 
 export default Products;
