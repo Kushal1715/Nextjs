df9ff221585475eb20c02bb70be203348ff47289
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-20 19:33:06 +0545

    fetching users using swrHook in client comp

--- next-js-course-2024/src/app/client-data-fetch/page.js
+++ next-js-course-2024/src/app/client-data-fetch/page.js
@@ -1,35 +1,50 @@
 'use client'
 import { useState, useEffect } from "react"
+import useSWR from "swr"
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function ClientSideDataFetching() {
-  const [loading, setLoading] = useState(false)
-  const [users, setUsers] = useState([])
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        setLoading(true)
-        const response = await fetch('https://dummyjson.com/users');
-        const data = await response.json();
 
-        if (data?.users) {
-          setUsers(data.users)
-        }
-        setLoading(false)
-      } catch (e) {
-        setLoading(false);
-        console.log(e)
-      }
-    }
-    fetchUsers();
-  }, [])
+  const { data, error, isLoading } = useSWR('https://dummyjson.com/users', fetcher)
 
-  if (loading) {
-    return <h3 className="text-center text-3xl">Loading..please wait</h3>
-  }
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
+  // const [loading, setLoading] = useState(false)
+  // const [users, setUsers] = useState([])
+
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     try {
+  //       setLoading(true)
+  //       const response = await fetch('https://dummyjson.com/users');
+  //       const data = await response.json();
+
+  //       if (data?.users) {
+  //         setUsers(data.users)
+  //       }
+  //       setLoading(false)
+  //     } catch (e) {
+  //       setLoading(false);
+  //       console.log(e)
+  //     }
+  //   }
+  //   fetchUsers();
+  // }, [])
+
+  // if (loading) {
+  //   return <h3 className="text-center text-3xl">Loading..please wait</h3>
+  // }
   return <div>
     <h1>Client side data fetching</h1>
-    {users.map(user => (
+    {/* {users.map(user => (
+      <div key={user.id}>
+        <p>{user.firstName}</p>
+      </div>
+    ))} */}
+    {data.users.map(user => (
       <div key={user.id}>
         <p>{user.firstName}</p>
       </div>
