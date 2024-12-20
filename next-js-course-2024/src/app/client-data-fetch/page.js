4869902fa5ea19d033944a1eab31edca552e4235
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-20 19:23:42 +0545

    fetching users in client side component

--- next-js-course-2024/src/app/client-data-fetch/page.js
+++ next-js-course-2024/src/app/client-data-fetch/page.js
@@ -1,5 +1,38 @@
+'use client'
+import { useState, useEffect } from "react"
+
 export default function ClientSideDataFetching() {
+  const [loading, setLoading] = useState(false)
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setLoading(true)
+        const response = await fetch('https://dummyjson.com/users');
+        const data = await response.json();
+
+        if (data?.users) {
+          setUsers(data.users)
+        }
+        setLoading(false)
+      } catch (e) {
+        setLoading(false);
+        console.log(e)
+      }
+    }
+    fetchUsers();
+  }, [])
+
+  if (loading) {
+    return <h3 className="text-center text-3xl">Loading..please wait</h3>
+  }
   return <div>
     <h1>Client side data fetching</h1>
+    {users.map(user => (
+      <div key={user.id}>
+        <p>{user.firstName}</p>
+      </div>
+    ))}
   </div>
 }
