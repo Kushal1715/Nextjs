137ea01d3b5e25f930043a988f1afac30c3ccddc
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-24 23:00:50 +0545

    using dialog from shadcn ui

--- server-actions_and_data-mutations/src/app/user-management/page.js
+++ server-actions_and_data-mutations/src/app/user-management/page.js
@@ -1,7 +1,15 @@
+import AddNewUser from "@/components/add-new-user";
 import React from "react";
 
 const UserManagement = () => {
-  return <div>UserManagement</div>;
+  return (
+    <div className="p-20 max-w-6xl">
+      <div className="flex justify-between">
+        <h1>User management</h1>
+        <AddNewUser />
+      </div>
+    </div>
+  );
 };
 
 export default UserManagement;
---
+++ server-actions_and_data-mutations/src/components/add-new-user/index.js
@@ -0,0 +1,54 @@
+"use client";
+import React, { useState } from "react";
+import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const AddNewUser = () => {
+  const [openDialog, setOpenDialog] = useState(false);
+  return (
+    <>
+      <div>
+        <Button onClick={() => setOpenDialog(true)}>Add New User</Button>
+      </div>
+      <Dialog open={openDialog} onOpenChange={() => setOpenDialog(false)}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <Input id="username" value="@peduarte" className="col-span-3" />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default AddNewUser;
