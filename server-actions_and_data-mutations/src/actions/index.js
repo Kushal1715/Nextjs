688257f098900900e83ba26338608ab853645734
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-25 22:18:07 +0545

    creating add new user action

--- server-actions_and_data-mutations/src/actions/index.js
+++ server-actions_and_data-mutations/src/actions/index.js
@@ -1,6 +1,34 @@
-'use server'
-export async function fetchProducts() {
-  const response = await fetch('https://dummyjson.com/products')
-  const data = await response.json()
-  return data?.products;
+"use server";
+
+import connectToDB from "@/database";
+import User from "@/models/user";
+
+// export async function fetchProducts() {
+//   const response = await fetch('https://dummyjson.com/products')
+//   const data = await response.json()
+//   return data?.products;
+// }
+
+export async function addNewUserAction(userFormData) {
+  try {
+    await connectToDB();
+    const newlyCreatedUser = await User.create(userFormData);
+    if (newlyCreatedUser) {
+      return {
+        success: true,
+        message: "New user added successfully",
+      };
+    } else {
+      return {
+        success: false,
+        message: "An error occurred while adding a new user",
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      message: "An error occurred while adding a new user",
+    };
+  }
 }
--- server-actions_and_data-mutations/src/utils/index.js
+++ server-actions_and_data-mutations/src/utils/index.js
@@ -3,20 +3,31 @@ export const userFormInput = [
     name: "firstName",
     label: "First Name",
     placeholder: "Enter your first name",
+    type: "input",
   },
   {
     name: "lastName",
     label: "Last Name",
     placeholder: "Enter your last name",
+    type: "input",
   },
   {
     name: "email",
     label: "Email",
     placeholder: "Enter your email",
+    type: "email",
   },
   {
     name: "address",
     label: "Address",
     placeholder: "Enter your address",
+    type: "input",
   },
 ];
+
+export const addNewUserInitialFormState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  address: "",
+};
