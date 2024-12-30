b779d2fb752632357944a0c87fbafe53bcf0fc70
Kushal1715 <kushalkhadka77@gmail.com>
2024-12-30 22:38:46 +0545

    making userRegistration form controls utils

--- authentication-and-middleware/src/app/sign-up/page.js
+++ authentication-and-middleware/src/app/sign-up/page.js
@@ -1,9 +1,22 @@
 "use client";
 
+import { Label } from "@/components/ui/label";
 import React from "react";
+import { userRegistrationFormControls } from "../utils";
 
 const SignUp = () => {
-  return <div>Welcome to sign up page</div>;
+  return (
+    <div>
+      <h1>Registration</h1>
+      <form>
+        {userRegistrationFormControls.map((controlItem) => (
+          <div>
+            <Label>{controlItem.label}</Label>
+          </div>
+        ))}
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
