8dc19d05771ef2ffdcfcba906dea29ea0d2cb36c
Kushal1715 <kushalkhadka77@gmail.com>
2025-04-27 16:06:17 +0545

    fetching current user

--- jop-portal-nextjs/src/app/page.js
+++ jop-portal-nextjs/src/app/page.js
@@ -1,3 +1,7 @@
-export default function Home() {
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  const user = await currentUser();
+  console.log(user);
   return <section>main content</section>;
 }
